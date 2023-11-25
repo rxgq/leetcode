ea68b8c318635023dae377b27463f74a2cdd57ea
Ashton Dunderdale <121163457+ashtondunderdale@users.noreply.github.com>
2023-11-25 13:52:43 +0000

    added ingredient data, get ingredient method

---
+++ snovita-tracker/Ingredient.cs
@@ -0,0 +1,54 @@
+﻿namespace snovita_tracker;
+
+internal class Ingredient
+{
+    public string Name { get; set; }
+    public int ServingSize { get; set; }
+    public NutritionInfo Nutrition { get; set; }
+
+    public Ingredient(string name, int servingSize, NutritionInfo nutrition)
+    {
+        Name = name;
+        ServingSize = servingSize;
+        Nutrition = nutrition;
+    }
+
+    public class NutritionInfo
+    {
+        public double Protein { get; set; }
+        public double Carbs { get; set; }
+        public double Fats { get; set; }
+        public double Sugars { get; set; }
+        public double Saturates { get; set; }
+        public double Fibre { get; set; }
+        public double Salt { get; set; }
+        public double Calories { get; set; }
+    }
+
+    public static Ingredient GetIngredient(int grams, string ingredient)
+    {
+        List<Ingredient> ingredientList = new()
+        {
+            new Ingredient(
+                "sultanas",
+                30,
+                new NutritionInfo
+                {
+                    Protein = 0.8,
+                    Carbs = 20.8,
+                    Fats = 0.1,
+                    Sugars = 20.8,
+                    Saturates = 0,
+                    Fibre = 0.6,
+                    Salt = 0.01,
+                    Calories = 89
+                }
+            ),
+        };
+
+        Ingredient foundIngredient = ingredientList.FirstOrDefault(i => i.Name == ingredient);
+
+        if (foundIngredient is not null) return foundIngredient;
+        throw new InvalidOperationException($"Ingredient '{ingredient}' not found.");
+    }
+}
--- snovita-tracker/Program.cs
+++ snovita-tracker/Program.cs
@@ -2,8 +2,32 @@
 
 internal class Program
 {
-    static void Main(string[] args)
+    static void Main() => Starter();
+
+    public static void Starter()
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Enter grams: ");
+
+        if (!int.TryParse(Console.ReadLine(), out int grams))
+        {
+            Console.WriteLine("Invalid input for grams. Please enter a valid integer.");
+            return;
+        }
+
+        Console.Write("Enter ingredient: ");
+        string? ingredient = Console.ReadLine();
+
+        if (ingredient is not null) 
+        {
+            Ingredient foundIngredient = Ingredient.GetIngredient(grams, ingredient);
+            CalculateMacros(grams, foundIngredient);
+        } 
+
+        else Console.WriteLine("Ingredient cannot be null.");
+    }
+
+    public static void CalculateMacros(int grams, Ingredient ingredient) 
+    {
+        Console.WriteLine(ingredient.Name);
     }
 }
