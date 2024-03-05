f38e5d9d507b2005d743dd1b5bb35c9dddcefe7c
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-03-05 21:43:45 +0000

    added build and populate grid

---
+++ snovita-tracker/Entity.cs
@@ -0,0 +1,7 @@
+﻿namespace life;
+
+internal class Entity
+{
+    public char SYMBOL = '0';
+    public ConsoleColor COLOUR = ConsoleColor.Blue;
+}
--- snovita-tracker/Program.cs
+++ snovita-tracker/Program.cs
@@ -2,13 +2,47 @@
 
 class Grid
 {
-    const int GRID_WIDTH = 20;
-    const int GRID_HEIGHT = 10;
+    const int GRID_X = 20;
+    const int GRID_Y = 10;
     const char GRID_CHAR = '.';
+
+    const int POPULATION = 10;
 
     static void Main() 
     {
-
+        BuildGrid();
+        PopulateGrid();
+        Console.ReadKey();
     }
 
+    static void BuildGrid()
+    {
+        for (int i = 0; i < GRID_Y; i++)
+        {
+            Console.Write(GRID_CHAR);
+
+            for (int j = 0; j < GRID_X; j++)
+            {
+                Console.Write(GRID_CHAR);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void PopulateGrid() 
+    {
+        Random rnd = new();
+
+
+        for (int i = 0; i < POPULATION; i++)
+        {
+            int randX = rnd.Next(GRID_X);
+            int randY = rnd.Next(GRID_Y);
+
+            Entity entity = new();
+
+            Console.SetCursorPosition(randX, randY);
+            Console.Write(entity.SYMBOL);
+        }
+    }
 }
