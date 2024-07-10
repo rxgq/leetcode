ca7f5e0f3fc8dd777b805b4d3a088e294cad1ae7
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-10 21:12:34 +0100

    two sum

---
+++ 1_two_sum.cs
@@ -0,0 +1,15 @@
+namespace leetcode;
+
+public partial class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.Length; i++) {
+            for (int j = 0; j < nums.Length; j++) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        return new int[0];
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿namespace leetcode;
+
+public class Program {
+    static void Main() {
+        var solution = new Solution();
+        solution.TwoSum(new int[] {2, 7, 11, 15}, 9);
+    }
+}
