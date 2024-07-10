892b00b8bcfb9d65bee95ec0e05de24f602ad2aa
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-10 22:00:55 +0100

    valid_parentheses

---
+++ 20_valid_parentheses.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode;
+
+public partial class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> stack = new Stack<char>();
+        if (s == null || s.Length < 2)
+            return false;
+
+        foreach (char c in s)
+        {
+            if (c == '(' || c == '{' || c == '[')
+                stack.Push(c);
+
+            else if (c == ')')
+                if (stack.Count == 0 || stack.Pop() != '(')
+                    return false;
+
+            else if (c == '}')
+                if (stack.Count == 0 || stack.Pop() != '{')
+                    return false;
+            
+            else if (c == ']')
+                if (stack.Count == 0 || stack.Pop() != '[')
+                    return false;
+        }
+
+        return stack.Count == 0;
+    }
+}
+
