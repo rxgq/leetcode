b8fedfb5f78378533290ffc0dc9cc91a8ba5cadd
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-20 17:53:31 +0100

    191

---
+++ 191.cs
@@ -0,0 +1,11 @@
+﻿namespace leetcode;
+
+public partial class Solution
+{
+    public int HammingWeight(int n)
+    {
+        int count = 0;
+        for (; n > 0; count += n & 1, n >>= 1) ;
+        return count;
+    }
+}
