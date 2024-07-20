aed99fe017013b435be8617b7ae7938290cab0a2
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-20 18:23:57 +0100

    3110

---
+++ 3110.cs
@@ -0,0 +1,15 @@
+﻿namespace leetcode;
+
+public partial class Solution
+{
+    public int ScoreOfString(string s)
+    {
+        int total = 0;
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            total += Math.Abs(s[i] - s[i + 1]);
+        }
+
+        return total;
+    }
+}
