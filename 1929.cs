eae7e845526a500df476376c7daca49bb34c2a33
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-20 18:37:43 +0100

    1929

---
+++ 1929.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode;
+
+public partial class Solution
+{
+    public int[] GetConcatenation(int[] nums)
+    {
+        int[] ans = new int[nums.Length * 2];
+
+        for (int i = 0; i < nums.Length; i++)
+            ans[i] = nums[i];
+
+        for (int i = 0; i < nums.Length; i++)
+            ans[nums.Length + i] = nums[i];
+
+        return ans;
+    }
+}
