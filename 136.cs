32cf1fd92e7393f69232afbab0b94f8a92efc9ad
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-20 18:04:26 +0100

    136

---
+++ 136.cs
@@ -0,0 +1,13 @@
+﻿namespace leetcode;
+
+public partial class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        int result = 0;
+        foreach (var num in nums)
+            result = num ^ result;
+
+        return result;
+    }
+}
