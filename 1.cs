7dd69fa6d7cbed9947283554ddcb20ed9a46c2d4
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-20 18:17:17 +0100

    169

--- 1.cs
+++ 1.cs
@@ -2,6 +2,7 @@ namespace leetcode;
 
 public partial class Solution {
     public int[] TwoSum(int[] nums, int target) {
+
         for (int i = 0; i < nums.Length; i++) {
             for (int j = 0; j < nums.Length; j++) {
                 if (nums[i] + nums[j] == target && i != j) {
---
+++ 169.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode;
+
+public partial class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        int result = 0;
+        int count = 0;
+
+        foreach (var num in nums)
+        {
+            if (count == 0)
+                result = num;
+
+            count += result == num ? 1 : -1;
+        }
+
+        return result;
+    }
+}
