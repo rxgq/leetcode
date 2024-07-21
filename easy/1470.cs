3d09d6612cc91e3694839a71d38cbcecd9e89c4e
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 10:33:16 +0100

    1470

---
+++ easy/1470.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int[] Shuffle(int[] nums, int n)
+    {
+        int[] x = new int[n * 2];
+
+        for (int i = 0; i < n; i++)
+        {
+            x[2 * i] = nums[i];
+            x[2 * i + 1] = nums[i + n];
+        }
+
+        return x;
+    }
+}
