c7e0417e4e3c43339c35c6bafadb180ee5c4f915
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 12:03:53 +0100

    876

---
+++ 876.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public partial class Solution
+{
+    public ListNode MiddleNode(ListNode head)
+    {
+        ListNode s = head;
+        ListNode f = head;
+
+        while (f != null && f.next != null)
+        {
+            s = s.next;
+            f = f.next.next;
+        }
+
+        return s;
+    }
+}
