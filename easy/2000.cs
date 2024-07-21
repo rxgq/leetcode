54433be1b0bc8af57124ed16b70da4e23e04e195
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 20:52:54 +0100

    2000

---
+++ easy/2000.cs
@@ -0,0 +1,15 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public string ReversePrefix(string word, char ch)
+    {
+        int index = word.IndexOf(ch);
+        if (index == -1) return word;
+
+        char[] arr = word.Substring(0, index + 1).ToCharArray();
+        Array.Reverse(arr);
+
+        return new string(arr) + word.Substring(index + 1);
+    }
+}
