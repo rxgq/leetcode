2978c39658a54aff599b332650a81c5a4dc6eb0f
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 22:30:48 +0100

    2535

---
+++ easy/2535.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int DifferenceOfSum(int[] nums)
+    {
+        int sum = 0;
+        int digitSum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+
+            string numStr = nums[i].ToString();
+            for (int j = 0; j < numStr.Length; j++)
+                digitSum += numStr[j] - '0';
+        }
+
+        return sum - digitSum;
+    }
+}
