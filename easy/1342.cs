588591000c23781014b4214cde372740a8d401bf
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 12:01:45 +0100

    1342

---
+++ easy/1342.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int NumberOfSteps(int num)
+    {
+        int steps = 0;
+
+        while (num != 0)
+        {
+            if (num % 2 == 0)
+            {
+                num /= 2;
+            }
+            else
+            {
+                num--;
+            }
+
+            steps++;
+        }
+
+        return steps;
+    }
+}
