bff71bd1097c973503f764bf9e072c7fb24910cd
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 21:53:01 +0100

    1688

---
+++ easy/1688.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int NumberOfMatches(int n)
+    {
+        int matches = 0;
+
+        while (n != 1)
+        {
+            if (n % 2 == 0)
+            {
+                matches += n / 2;
+                n = n / 2;
+            }
+            else
+            {
+                matches += (n - 1) / 2;
+                n = (n - 1) / 2 + 1;
+            }
+        }
+
+        return matches;
+        // alternatively: return n - 1;
+    }
+}
