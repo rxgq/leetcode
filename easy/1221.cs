7f3f29765f14c280be4c678ba96dc37649739305
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 21:04:20 +0100

    1221

---
+++ easy/1221.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int BalancedStringSplit(string s)
+    {
+        int balance = 0;
+        int count = 0;
+
+        foreach (char c in s)
+        {
+            balance += c == 'R' ? 1 : -1;
+            if (balance == 0) count++;
+        }
+
+        return count;
+    }
+}
