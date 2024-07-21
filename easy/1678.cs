859cb51e40706a69e7171585d59744ee90c4f60b
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 19:49:16 +0100

    1678

---
+++ easy/1678.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public string Interpret(string command)
+    {
+        char[] result = new char[command.Length];
+        int index = 0;
+
+        for (int i = 0; i < command.Length;)
+        {
+            if (command[i] == 'G')
+            {
+                result[index++] = 'G';
+                i++;
+            }
+            else if (command[i] == '(' && command[i + 1] == ')')
+            {
+                result[index++] = 'o';
+                i += 2;
+            }
+            else
+            {
+                result[index++] = 'a';
+                result[index++] = 'l';
+                i += 4;
+            }
+        }
+
+        return new string(result, 0, index);
+    }
+}
