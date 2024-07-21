9ccf2321d9cebb8200ec461cd4b49c0cc0db9f08
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 19:54:55 +0100

    2824

---
+++ easy/2824.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int CountPairs(IList<int> nums, int target)
+    {
+        int c = 0;
+        for (int i = 0; i < nums.Count; i++)
+            for (int j = 0; j < nums.Count; j++)
+                if (nums[i] + nums[j] < target && 0 <= i && i < j && j < nums.Count)
+                {
+                    c++;
+                }
+
+        return c;
+    }
+}
