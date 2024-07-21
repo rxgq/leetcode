8d7f4e1672cc714e270c18c7b1614e84a5657c7a
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 09:54:50 +0100

    1920

---
+++ easy/1920.cs
@@ -0,0 +1,16 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int[] BuildArray(int[] nums)
+    {
+        int[] ns = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            ns[i] = nums[nums[i]];
+        }
+
+        return ns;
+    }
+}
