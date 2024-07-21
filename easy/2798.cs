b492bd2cf6cba187e935751329fb3c1bf9831ddd
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 19:36:57 +0100

    2798

---
+++ easy/2798.cs
@@ -0,0 +1,14 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+    {
+        int count = 0;
+        for (int i = 0; i < hours.Length; i++)
+            if (hours[i] >= target)
+                count++;
+
+        return count;
+    }
+}
