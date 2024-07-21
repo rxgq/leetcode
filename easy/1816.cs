82d7bfe6c4efabc3bfef8d1e88b013eb70c1b652
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 22:07:41 +0100

    1816

---
+++ easy/1816.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace leetcode.easy;
+
+public partial class Solution
+{
+    public string TruncateSentence(string s, int k)
+    {
+        string[] words = s.Split(' ');
+        var result = new StringBuilder();
+
+        for (int i = 0; i < k; i++)
+        {
+            result.Append(i == 0 ? "" : " ");
+            result.Append(words[i]);
+        }
+
+        return result.ToString();
+    }
+}
