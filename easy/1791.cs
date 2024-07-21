a678722864d3f6f18975610474c8b43e494ac64b
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 20:12:58 +0100

    1791

---
+++ easy/1791.cs
@@ -0,0 +1,16 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int FindCenter(int[][] edges)
+    {
+        if (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1])
+        {
+            return edges[0][0];
+        }
+        else
+        {
+            return edges[0][1];
+        }
+    }
+}
