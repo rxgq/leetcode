71df052023c3afd19c38975a966c40c2314b30fc
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 20:30:13 +0100

    2114

---
+++ easy/2114.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int MostWordsFound(string[] sentences)
+    {
+        int maxCount = 0;
+        for (int i = 0; i < sentences.Length; i++)
+        {
+            string[] words = sentences[i].Split(" ");
+
+            if (words.Length > maxCount)
+                maxCount = words.Length;
+        }
+
+        return maxCount;
+    }
+}
