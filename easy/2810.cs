f603dec7ad96b0d8c1a16548eb32a3904a132e48
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 23:21:33 +0100

    2810

---
+++ easy/2810.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public string FinalString(string s)
+    {
+        string typedString = "";
+
+        foreach (char c in s)
+        {
+            if (c == 'i')
+            {
+                char[] arr = typedString.ToCharArray();
+                Array.Reverse(arr);
+                typedString = new string(arr);
+            }
+            else
+            {
+                typedString += c;
+            }
+        }
+
+        return typedString;
+    }
+}
