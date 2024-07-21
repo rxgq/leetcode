f00869bbbcbaca1c25d2ce3c89d556b8208f4fb2
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 00:28:19 +0100

    2828

---
+++ easy/2828.cs
@@ -0,0 +1,13 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public bool IsAcronym(IList<string> words, string s)
+    {
+        string acr = "";
+        foreach (string word in words)
+            acr += word[0];
+
+        return acr == s;
+    }
+}
