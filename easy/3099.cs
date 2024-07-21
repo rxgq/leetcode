30c4bcf285bc325b8667a3a51d81f7381be46a1c
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 00:36:16 +0100

    3099

---
+++ easy/3099.cs
@@ -0,0 +1,15 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int SumOfTheDigitsOfHarshadNumber(int x)
+    {
+        string xStr = x.ToString();
+
+        int xSum = 0;
+        foreach (char c in xStr)
+            xSum += c - '0';
+
+        return x % xSum != 0 ? -1 : xSum;
+    }
+}
