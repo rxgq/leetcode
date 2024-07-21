61d7aa9f53bc2fe2335c445e3148425f420a6244
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 20:34:23 +0100

    3162

---
+++ easy/3162.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int NumberOfPairs(int[] nums1, int[] nums2, int k)
+    {
+        int count = 0;
+
+        for (int i = 0; i < nums1.Length; i++)
+        {
+            for (int j = 0; j < nums2.Length; j++)
+            {
+                if (nums1[i] % (nums2[j] * k) == 0) count++;
+            }
+        }
+
+        return count;
+    }
+}
