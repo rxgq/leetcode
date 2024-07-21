c6f14b1f8e8239180db5fc3493bd3719f887467c
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 09:21:45 +0100

    2894

---
+++ 2011.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode;
+
+public partial class Solution
+{
+    public int FinalValueAfterOperations(string[] operations)
+    {
+        int x = 0;
+
+        foreach (var op in operations)
+        {
+            if (op[0] == '+' || op[2] == '+')
+                x++;
+
+            else
+                x--;
+        }
+
+        return x;
+    }
+}
