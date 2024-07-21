f97f439c312a97fc5e6bea34e319fb5d72f5581f
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 00:08:46 +0100

    557

---
+++ easy/557.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public string ReverseWords(string s)
+    {
+        string[] words = s.Split(" ");
+        string[] reversedWords = new string[words.Length];
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            char[] chars = words[i].ToCharArray();
+            Array.Reverse(chars);
+            reversedWords[i] = new string(chars);
+        }
+
+        return string.Join(" ", reversedWords);
+    }
+}
