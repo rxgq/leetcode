62ae7e317f332ed43495820d9d88e65a6a9e6b7b
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 00:46:15 +0100

    1913

---
+++ easy/1913.cs
@@ -0,0 +1,15 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int MaxProductDifference(int[] nums)
+    {
+        Array.Sort(nums);
+        int n = nums.Length;
+
+        int maxProduct = nums[n - 1] * nums[n - 2];
+        int minProduct = nums[0] * nums[1];
+
+        return maxProduct - minProduct;
+    }
+}
