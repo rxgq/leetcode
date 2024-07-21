bdfd7ab4369603942e510ec644a47fa9cd6b0f74
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 00:17:53 +0100

    804

---
+++ easy/804.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int UniqueMorseRepresentations(string[] words)
+    {
+        var set = new HashSet<string>();
+
+        string[] morse = new string[] {
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+        "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+        "..-", "...-", ".--", "-..-", "-.--", "--.."
+    };
+
+        foreach (string word in words)
+        {
+            var sb = new StringBuilder();
+
+            foreach (char c in word)
+            {
+                int index = c - 'a';
+                sb.Append(morse[index]);
+            }
+
+            set.Add(sb.ToString());
+        }
+
+        return set.Count;
+    }
+}
