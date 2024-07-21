aa938811381c793c6012a9129ff6008e65ac2d6d
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 21:58:48 +0100

    1662

---
+++ easy/1662.cs
@@ -0,0 +1,12 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+    {
+        string x = string.Join("", word1);
+        string y = string.Join("", word2);
+
+        return x == y;
+    }
+}
