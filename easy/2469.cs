6f0b4fe4e9f913f94df549ef61bec8b3215ad9fc
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 10:04:37 +0100

    3190

---
+++ easy/2469.cs
@@ -0,0 +1,9 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public double[] ConvertTemperature(double celsius)
+    {
+        return [celsius + 273.15, celsius * 1.8 + 32];
+    }
+}
---
+++ easy/3190.cs
@@ -0,0 +1,16 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int MinimumOperations(int[] nums)
+    {
+        int ops = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] % 3 != 0) ops++;
+        }
+
+        return ops;
+    }
+}
