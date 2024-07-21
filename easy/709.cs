0eb5c97a739a76110c53d1e6225d380c399b0865
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 23:29:14 +0100

    709

---
+++ easy/709.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public string ToLowerCase(string s)
+    {
+        var sb = new StringBuilder();
+
+        foreach (char c in s)
+        {
+            if (c >= 'A' && c <= 'Z')
+            {
+                int lower = ((int)c) + 32;
+                sb.Append(Convert.ToChar(lower));
+            }
+            else
+            {
+                sb.Append(c);
+            }
+
+        }
+
+        return sb.ToString();
+    }
+}
