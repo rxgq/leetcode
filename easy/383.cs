311721341bbb4103dda0d00e8e23a23bd803a3d3
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 12:07:19 +0100

    383

---
+++ easy/383.cs
@@ -0,0 +1,15 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        List<char> list = new List<char>(magazine);
+
+        for (int i = 0; i < ransomNote.Length; i++)
+            if (!list.Remove(ransomNote[i]))
+                return false;
+
+        return true;
+    }
+}
