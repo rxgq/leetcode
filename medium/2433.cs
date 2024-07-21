bbd004a2da5c2c4e9c7ee2e33946958fd9c288eb
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 11:10:44 +0100

    2433

---
+++ medium/2433.cs
@@ -0,0 +1,16 @@
+﻿namespace leetcode.medium;
+
+public partial class Solution
+{
+    public int[] FindArray(int[] pref)
+    {
+        int n = pref.Length;
+        int[] arr = new int[n];
+
+        arr[0] = pref[0];
+        for (int i = 1; i < n; i++)
+            arr[i] = pref[i] ^ pref[i - 1];
+
+        return arr;
+    }
+}
