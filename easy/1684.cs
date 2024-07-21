0e597ac34dc0cee77c49b04c7cf6bf117f89f977
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 23:40:55 +0100

    1684

---
+++ easy/1684.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int CountConsistentStrings(string allowed, string[] words)
+    {
+        int num = 0;
+
+        foreach (string word in words)
+        {
+            bool isConsistent = true;
+
+            foreach (char c in word)
+            {
+                if (!allowed.Contains(c))
+                {
+                    isConsistent = false;
+                    break;
+                }
+            }
+
+            if (isConsistent) num++;
+        }
+
+        return num;
+    }
+}
