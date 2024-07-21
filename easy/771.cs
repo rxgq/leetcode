d788f5fcfc8aff77ec014b7aa3561c75f99b39b2
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 10:08:52 +0100

    771

---
+++ easy/771.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        int count = 0;
+
+        for (int i = 0; i < jewels.Length; i++)
+        {
+            for (int j = 0; j < stones.Length; j++)
+                if (stones[j] == jewels[i])
+                    count++;
+        }
+
+        return count;
+    }
+}
