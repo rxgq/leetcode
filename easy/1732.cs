73d0beb0268b33ac99c1ae42e0e3580dbadc3876
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 23:44:16 +0100

    1732

---
+++ easy/1732.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int LargestAltitude(int[] gain)
+    {
+        int max = 0;
+        int altitude = 0;
+
+        foreach (var alt in gain)
+        {
+            altitude += alt;
+            max = Math.Max(altitude, max);
+        }
+
+        return max;
+    }
+}
