70d194ab33117ab24fa651b09f583f903d271df6
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 10:42:06 +0100

    1512

---
+++ easy/1512.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int NumIdenticalPairs(int[] nums)
+    {
+        var count = new Dictionary<int, int>();
+        int result = 0;
+
+        foreach (var num in nums)
+        {
+            if (count.ContainsKey(num))
+            {
+                result += count[num];
+                count[num]++;
+            }
+            else
+                count[num] = 1;
+        }
+
+        return result;
+    }
+}
