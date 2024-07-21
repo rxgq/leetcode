475cf238929e58afd4e8a2cb9f8d146abd04055d
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 22:16:52 +0100

    2520

---
+++ easy/2520.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int CountDigits(int num)
+    {
+        string numStr = num.ToString();
+        int count = 0;
+
+        for (int i = 0; i < numStr.Length; i++)
+        {
+            int digit = numStr[i] - '0';
+
+            if (num % digit == 0)
+                count++;
+        }
+
+        return count;
+    }
+}
