dc9ac53078aa3da18c27b68930daa29861967224
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 00:55:37 +0100

    1464

---
+++ easy/1464.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int MaxProduct(int[] nums)
+    {
+        int max1 = 0;
+        int max2 = 0;
+
+        foreach (int num in nums)
+        {
+            if (num > max1)
+            {
+                max2 = max1;
+                max1 = num;
+            }
+            else if (num > max2)
+            {
+                max2 = num;
+            }
+        }
+
+        return (max1 - 1) * (max2 - 1);
+    }
+}
