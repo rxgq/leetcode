a43a28e2ae1a18b878438788e68d6dea89fbd305
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 22:45:56 +0100

    2485

---
+++ easy/2485.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int PivotInteger(int n)
+    {
+        int totalSum = n * (n + 1) / 2;
+
+        for (int x = 1; x <= n; x++)
+        {
+            int sum1ToX = x * (x + 1) / 2;
+            int sumXToN = totalSum - (x - 1) * x / 2;
+
+            if (sum1ToX == sumXToN) return x;
+        }
+
+        return -1;
+    }
+}
