f4f6efaad0904b9e774ea53f2baa29b405af9c7d
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 21:21:05 +0100

    1486

---
+++ easy/1486.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int XorOperation(int n, int start)
+    {
+        int xor = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int num = start + 2 * i;
+            xor ^= num;
+        }
+
+        return xor;
+    }
+}
