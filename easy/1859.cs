22bb2c397d7b5546e3410e4f788264eeee4421fa
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 00:05:00 +0100

    1859

---
+++ easy/1859.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public string SortSentence(string s)
+    {
+        string[] words = s.Split(" ");
+        string[] sortedWords = new string[words.Length];
+
+        foreach (string word in words)
+        {
+            int index = word[word.Length - 1] - '0';
+            string w = word.Substring(0, word.Length - 1);
+            sortedWords[index - 1] = w;
+        }
+
+        return string.Join(" ", sortedWords);
+    }
+}
