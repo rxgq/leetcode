7b8b6208b5e432dfd5059a81e7168c8c60967fa4
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 20:25:44 +0100

    1281

---
+++ easy/1281.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int SubtractProductAndSum(int n)
+    {
+        int product = 1;
+        int sum = 0;
+
+        string nStr = n.ToString();
+
+        for (int i = 0; i < nStr.Length; i++)
+            product *= int.Parse(nStr[i].ToString());
+
+        for (int i = 0; i < nStr.Length; i++)
+            sum += int.Parse(nStr[i].ToString());
+
+        return product - sum;
+    }
+}
