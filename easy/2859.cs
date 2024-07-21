b1e63ea07c34eec8e243318adc1ecce061556023
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 20:20:13 +0100

    2859

--- easy/2859.cs
+++ easy/2859.cs
@@ -1,1 +1,21 @@
-﻿
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int SumIndicesWithKSetBits(IList<int> nums, int k)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.Count; i++)
+        {
+            string binary = Convert.ToString(i, 2);
+            int setBits = 0;
+
+            foreach (char s in binary)
+                if (s == '1') setBits++;
+
+            if (setBits == k) count += nums[i];
+        }
+
+        return count;
+    }
+}
