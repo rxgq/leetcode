48737711c585f3f92172dd955b18d0c5c87376e4
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 09:16:52 +0100

    1108

---
+++ 1108.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode;
+
+public partial class Solution
+{
+    public string DefangIPaddr(string address)
+    {
+        string defanged = "";
+
+        for (int i = 0; i < address.Length; i++)
+        {
+            if (address[i] == '.')
+            
+                defanged += "[.]";
+
+            else
+                defanged += address[i];
+        }
+
+        return defanged;
+    }
+}
