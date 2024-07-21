017fcdcb38058c8e55c75a7d2d2c239dbb307af0
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 23:14:10 +0100

    1614

---
+++ easy/1614.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int MaxDepth(string s)
+    {
+        int max = 0;
+        int depth = 0;
+
+        foreach (char c in s)
+        {
+            if (c == '(') max = Math.Max(max, ++depth);
+            if (c == ')') depth--;
+        }
+
+        return max;
+    }
+}
