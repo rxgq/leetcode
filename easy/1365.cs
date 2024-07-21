c2802806c71f72717d59625f2703b83a452e754f
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 19:59:59 +0100

    1365

---
+++ easy/1365.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int[] SmallerNumbersThanCurrent(int[] nums)
+    {
+        int[] arr = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int count = 0;
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] < nums[i])
+                    count++;
+
+                arr[i] = count;
+            }
+        }
+
+        return arr;
+    }
+}
