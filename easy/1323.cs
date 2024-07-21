90c0cec4e0ad12c9bee1cf28fb0052491da84124
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 00:40:52 +0100

    1323

---
+++ easy/1323.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int Maximum69Number(int num)
+    {
+        char[] s = num.ToString().ToCharArray();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '6')
+            {
+                s[i] = '9';
+                return int.Parse(s);
+            }
+        }
+
+        return num;
+    }
+}
