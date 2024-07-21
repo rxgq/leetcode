f337c58d5ccb78bec72db061c21026062e740169
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 21:28:15 +0100

    3065

--- easy/3065.cs
+++ easy/3065.cs
@@ -1,1 +1,17 @@
-﻿
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int MinOperations(int[] nums, int k)
+    {
+        Array.Sort(nums);
+        Array.Reverse(nums);
+
+        int count = 0;
+        foreach (int i in nums)
+            if (i < k)
+                count++;
+
+        return count;
+    }
+}
