8f3c25f8ad09b5256d5c93aae3bd3246aaa26e45
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 21:14:02 +0100

    2160

---
+++ easy/2160.cs
@@ -0,0 +1,16 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int MinimumSum(int num)
+    {
+        char[] digits = num.ToString().ToCharArray();
+
+        Array.Sort(digits);
+
+        int new1 = (digits[0] - '0') * 10 + (digits[2] - '0');
+        int new2 = (digits[1] - '0') * 10 + (digits[3] - '0');
+
+        return new1 + new2;
+    }
+}
