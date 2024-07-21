c0d58195a3458e898ade3f659f86a77e9fad81b5
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 21:43:05 +0100

    2974

---
+++ easy/2974.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int[] NumberGame(int[] nums)
+    {
+        Array.Sort(nums);
+
+        for (int i = 0; i <= nums.Length - 2; i += 2)
+        {
+            int temp = nums[i];
+            nums[i] = nums[i + 1];
+            nums[i + 1] = temp;
+        }
+
+        return nums;
+    }
+}
