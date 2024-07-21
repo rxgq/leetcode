e6798c585bcf7713986d975441a0d667586a5f0c
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 00:25:43 +0100

    2864

---
+++ easy/2864.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public string MaximumOddBinaryNumber(string s)
+    {
+        int oneCount = 0;
+        int zeroCount = 0;
+
+        foreach (char c in s)
+            if (c == '1') oneCount++;
+            else zeroCount++;
+
+        string result = new string('1', oneCount - 1) + new string('0', zeroCount) + "1";
+        return result;
+    }
+}
