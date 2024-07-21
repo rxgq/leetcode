3b5a70102275b16f06d6b67e108427776fec3224
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-21 10:15:18 +0100

    2942

---
+++ easy/2942.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public IList<int> FindWordsContaining(string[] words, char x)
+    {
+        var nums = new List<int>();
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Contains(x))
+            {
+                nums.Add(i);
+            }
+        }
+
+        return nums;
+    }
+}
