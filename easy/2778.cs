846acf8b5d36a990cbb9f8fb286f8402f3191ad4
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 01:31:34 +0100

    2778

---
+++ easy/2778.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int SumOfSquares(int[] nums)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums.Length % (i + 1) == 0)
+                sum += (nums[i] * nums[i]);
+        }
+
+        return sum;
+    }
+}
