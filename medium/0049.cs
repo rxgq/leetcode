d779b68a70291983479fbf427b9d411781372618
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 17:40:25 +0100

    49

---
+++ medium/0049.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode.medium;
+
+public partial class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        var map = new Dictionary<string, IList<string>>();
+
+        foreach (var str in strs)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Sort(charArray);
+            string key = new string(charArray);
+
+            if (!map.ContainsKey(key))
+                map[key] = new List<string>();
+
+            map[key].Add(str);
+        }
+
+        return new List<IList<string>>(map.Values);
+    }
+}
