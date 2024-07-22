1e4ac633c4633d6db46a77afec8538fcd9d2a1e9
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 21:02:25 +0100

    1603

---
+++ easy/1603.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.easy
+{
+    public class ParkingSystem {
+
+        public int b;
+        public int m;
+        public int s;
+
+        public ParkingSystem(int big, int medium, int small) {
+            b = big;
+            m = medium;
+            s = small;
+        }
+    
+        public bool AddCar(int carType) {
+            if (carType == 1) {
+                if (b == 0) return false;
+                b--;
+            }
+
+            if (carType == 2) {
+                if (m == 0) return false;
+                m--;
+            }
+
+            if (carType == 3) {
+                if (s == 0) return false;
+                s--;
+            }
+
+            return true;
+        }
+    }
+}
