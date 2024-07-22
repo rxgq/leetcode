d910b7bad2d49d5450e31dfbd08da812ac0671e7
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 17:26:54 +0100

    217

---
+++ easy/0217.cs
@@ -0,0 +1,17 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var hash = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!hash.Add(nums[i]))
+                return true;
+        }
+
+        return false;
+    }
+}
