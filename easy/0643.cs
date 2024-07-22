c0bfc5965ee443ace42ca2917fe59816192029c6
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 14:59:17 +0100

    643

---
+++ easy/0643.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        double currentSum = 0;
+        for (int i = 0; i < k; i++)
+        {
+            currentSum += nums[i];
+        }
+
+        double maxSum = currentSum;
+
+        for (int i = k; i < nums.Length; i++)
+        {
+            currentSum = currentSum - nums[i - k] + nums[i];
+            if (currentSum > maxSum) maxSum = currentSum;
+        }
+
+        return maxSum / k;
+    }
+}
