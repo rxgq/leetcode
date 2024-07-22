6167cdbb2fd860d3abc4f8fb50d31c1e78905e95
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 21:13:30 +0100

    1863

---
+++ easy/1863.cs
@@ -0,0 +1,12 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int SubsetXORSum(int[] nums)
+    {
+        int x = 0;
+        foreach (int num in nums) x |= num;
+
+        return x << (nums.Length - 1);
+    }
+}
