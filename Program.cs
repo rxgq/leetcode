acb8d2e916b5d8f79502ddd3abcadd88fdba64b6
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 16:44:33 +0100

    704

--- Program.cs
+++ Program.cs
@@ -3,6 +3,17 @@
 public class Program {
     static void Main() {
         var solution = new Solution();
-        solution.CheckIfPanagram("thequickbrownfoxjumpsoverthelazydog");
+
+        int[] arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        int target = 6;
+
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+
+        }
     }
 }
