d8ec79860faf904553ce7c6546a6aa1334354cf4
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 14:45:30 +0100

    594

---
+++ easy/0219.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        var set = new HashSet<int>();
+
+        int left = 0;
+        for (int right = 0; right < nums.Length; right++)
+        {
+            if (right - left > k)
+            {
+                set.Remove(nums[left]);
+                left++;
+            }
+
+            if (set.Contains(nums[right])) return true;
+            set.Add(nums[right]);
+        }
+
+        return false;
+    }
+}
---
+++ easy/0594.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int FindLHS(int[] nums)
+    {
+        Array.Sort(nums);
+
+        int left = 0; int right = 1;
+        int max = 0;
+        while (right < nums.Length)
+        {
+            if (nums[right] - nums[left] == 1)
+            {
+                max = Math.Max(max, right - left + 1);
+            }
+            else if (nums[right] - nums[left] > 1)
+            {
+                left++;
+                continue;
+            }
+
+            right++;
+        }
+
+        return max;
+    }
+}
