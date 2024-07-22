3fa01f93f07a65406fa79132ae0b296bb52f8b35
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 01:17:31 +0100

    3131

---
+++ easy/3131.cs
@@ -0,0 +1,13 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int AddedInteger(int[] nums1, int[] nums2)
+    {
+        Array.Sort(nums1);
+        Array.Sort(nums2);
+
+        int x = nums2[0] - nums1[0];
+        return x;
+    }
+}
