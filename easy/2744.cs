cb8ff1517c26625e9891c1adf1e6fa4802700fea
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 01:25:00 +0100

    2744

---
+++ easy/2744.cs
@@ -0,0 +1,27 @@
+﻿
+namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int MaximumNumberOfStringPairs(string[] words)
+    {
+        int count = 0;
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            for (int j = i + 1; j < words.Length; j++)
+            {
+                char[] jArr = words[j].ToCharArray();
+                Array.Reverse(jArr);
+                string jStr = new string(jArr);
+
+                if (jStr == words[i])
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
