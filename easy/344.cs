2b0a7dd622b86af52e008683cf545bb8012cb07c
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 17:04:11 +0100

    344

---
+++ easy/344.cs
@@ -0,0 +1,14 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        int l = 0; int r = s.Length - 1;
+        while (l < r)
+        {
+            (s[l], s[r]) = (s[r], s[l]);
+            l++; r--;
+        }
+    }
+}
