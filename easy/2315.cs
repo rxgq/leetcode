19fbc6a005378a4f703c23f200216b9a3ab21292
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 01:02:43 +0100

    2315

---
+++ easy/2315.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int CountAsterisks(string s)
+    {
+        int count = 0;
+        bool isIncluded = true;
+
+        foreach (char c in s)
+        {
+            if (c == '|')
+                isIncluded = !isIncluded;
+
+            if (c == '*' && isIncluded)
+                count++;
+        }
+
+        return count;
+    }
+}
