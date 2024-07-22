24bdcc6be55d0d36ec38987503b9bf2a28caa59d
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 20:10:41 +0100

    125

---
+++ easy/0125.cs
@@ -0,0 +1,27 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        s = s.ToLower();
+
+        StringBuilder sb = new StringBuilder();
+        foreach (char c in s)
+            if (Char.IsLetterOrDigit(c))
+                sb.Append(c);
+
+        s = sb.ToString();
+
+        int left = 0;
+        int right = s.Length - 1;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[left] != s[right]) return false;
+            left++; right--;
+        }
+
+        return true;
+    }
+}
