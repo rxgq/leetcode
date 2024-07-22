83116c3a84e2a2c7dd66448225c98ef4a9d529c5
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 20:47:34 +0100

    2373

---
+++ easy/2373.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int[][] LargestLocal(int[][] grid)
+    {
+        int n = grid.Length;
+        int[][] maxLocal = new int[n - 2][];
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            maxLocal[i] = new int[n - 2];
+
+            for (int j = 0; j < n - 2; j++)
+            {
+                int maxVal = int.MinValue;
+
+                for (int x = i; x < i + 3; x++)
+                    for (int y = j; y < j + 3; y++)
+                        if (grid[x][y] > maxVal) maxVal = grid[x][y];
+
+                maxLocal[i][j] = maxVal;
+            }
+        }
+
+        return maxLocal;
+    }
+}
