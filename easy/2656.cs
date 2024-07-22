340da3a7cb7846f2fc9744f6ce4f59d0476bd9d3
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-22 01:08:32 +0100

    2656

---
+++ easy/2656.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public int MaximizeSum(int[] nums, int k)
+    {
+        Array.Sort(nums);
+
+        int maxElement = nums[nums.Length - 1];
+        int score = 0;
+
+        for (int i = 0; i < k; i++)
+        {
+            score += maxElement;
+            maxElement++;
+        }
+
+        return score;
+    }
+}
