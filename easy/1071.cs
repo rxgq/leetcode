b187fc6ab12861b6647215a974f8e2c5cc1ef228
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-25 21:42:42 +0100

    1071

---
+++ easy/1071.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public string GcdOfStrings(string str1, string str2)
+    {
+        int Gcd(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        if (str1 + str2 != str2 + str1)
+            return "";
+
+        int gcd = Gcd(str1.Length, str2.Length);
+        return str1.Substring(0, gcd);
+    }
+}
--- easy/2652.cs
+++ easy/2652.cs
@@ -9,9 +9,7 @@ public partial class Solution
         for (int i = 0; i < n + 1; i++)
         {
             if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
-            {
                 sum += i;
-            }
         }
 
         return sum;
