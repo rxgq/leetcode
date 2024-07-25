d43f1eef5656f9a982876740068ab86cf2c4bec0
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-25 22:12:34 +0100

    345

---
+++ easy/0345.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public string ReverseVowels(string s)
+    {
+        if (s == null || s.Length == 0) return s;
+
+        int i = 0; int j = s.Length - 1;
+        string vowels = "aeiouAEIOU";
+
+        char[] arr = s.ToCharArray();
+
+        while (i < j)
+        {
+            if (!vowels.Contains(arr[i]))
+                i++;
+            else if (!vowels.Contains(arr[j]))
+                j--;
+
+            else
+            {
+                (arr[j], arr[i]) = (arr[i], arr[j]);
+                i++; j--;
+            }
+        }
+
+        return new string(arr);
+    }
+}
