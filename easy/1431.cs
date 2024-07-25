03399f9175d60c870671f530facf3afbed85d585
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-25 21:49:58 +0100

    1431

---
+++ easy/1431.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        List<bool> result = new();
+
+        int greatest = 0;
+        for (int i = 0; i < candies.Length; i++)
+            greatest = Math.Max(greatest, candies[i]);
+
+        for (int i = 0; i < candies.Length; i++)
+            result.Add((candies[i] + extraCandies) >= greatest);
+
+        return result;
+    }
+}
