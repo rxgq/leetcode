90f448a73486ec9e6edfc57ef29e391f4b5109cd
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-25 21:24:57 +0100

    1768

---
+++ easy/1768.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode.easy;
+
+public partial class Solution
+{
+    public string MergeAlternately(string word1, string word2)
+    {
+        string merged = "";
+
+        int i = 0; int j = 0;
+        while (i < word1.Length && j < word2.Length)
+        {
+            merged += word1[i];
+            merged += word2[j];
+
+            i++; j++;
+        }
+
+        merged += word1[i..];
+        merged += word2[j..];
+
+        return merged;
+    }
+}
