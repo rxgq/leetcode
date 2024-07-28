07780e7e664f465b414d9cd86e181d8ac62d3ed7
rxgq <ashtondunderdale@gmail.com>
2024-07-28 14:04:46 +0100

    2418

---
+++ easy/2418.cs
@@ -0,0 +1,17 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public string[] SortPeople(string[] names, int[] heights) {
+        (string name, int height)[] people = new (string, int)[names.Length];
+        for (int i = 0; i < names.Length; i++)
+          people[i] = (names[i], heights[i]);
+
+        Array.Sort(people, (a, b) => b.height.CompareTo(a.height));
+
+        string[] result = new string[names.Length];
+        for (int i = 0; i < names.Length; i++)
+            result[i] = people[i].name;
+
+        return result;
+    }
+}
