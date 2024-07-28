247b47e6eb439d86a9d6ef01068dc8687eca8469
rxgq <ashtondunderdale@gmail.com>
2024-07-28 15:19:20 +0100

    2053

---
+++ easy/2053.cs
@@ -0,0 +1,20 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public string KthDistinct(string[] arr, int k) {
+        var seen = new HashSet<string>();
+        var unique = new List<string>();
+
+        for (int i = 0; i < arr.Length; i++) {
+          if (seen.Contains(arr[i])) {
+            unique.Remove(arr[i]);
+          } else {
+            unique.Add(arr[i]);
+            seen.Add(arr[i]);
+          }
+        }
+              
+        if (k - 1 < unique.Count) return unique[k - 1];
+        return "";
+    }
+}
