617f5a956a9f14b28c505f6bf44376912ea3bad8
rxgq <ashtondunderdale@gmail.com>
2024-07-28 17:14:58 +0100

    2391

---
+++ medium/2391.cs
@@ -0,0 +1,27 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int GarbageCollection(string[] garbage, int[] travel) {
+
+      int Collect(char g) {
+        int sum = 0;
+        int lastIndex = -1;
+
+        for (int i = 0; i < garbage.Length; i++) {
+          foreach (char c in garbage[i]) {
+            if (c == g) {
+              sum++;
+              lastIndex = i;
+            }
+          }
+        }
+
+        for (int j = 0; j < lastIndex; j++)
+          sum += travel[j];
+w
+        return sum;
+      }
+      
+      return Collect('G') + Collect('P') + Collect('M');
+    }
+}
