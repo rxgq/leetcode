5a475a643a734f97d44c76010fb1846b667474bd
rxgq <ashtondunderdale@gmail.com>
2024-07-28 14:13:46 +0100

    1528

---
+++ easy/1528.cs
@@ -0,0 +1,12 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public string RestoreString(string s, int[] indices) {
+      char[] result = new char[s.Length];
+
+      for (int i = 0; i < s.Length; i++)
+        result[indices[i]] = s[i]; 
+
+      return new string(result);
+    }
+}
