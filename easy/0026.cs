1d7f839db187d8d9ef95ebdad7a8b0915cd2a276
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-07-29 14:06:48 +0100

    26

---
+++ easy/0026.cs
@@ -0,0 +1,15 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 1; j < nums.Length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
+    }
+}
