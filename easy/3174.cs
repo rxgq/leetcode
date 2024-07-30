553891b2dae587782616d423194961c674565a71
rxgq <ashtondunderdale@gmail.com>
2024-07-30 19:00:37 +0100

    3174

---
+++ easy/3174.cs
@@ -0,0 +1,17 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public string ClearDigits(string s) {
+        var ans = new List<char>();
+
+        foreach (char c in s) {
+          if (char.IsDigit(c)) {
+            ans.RemoveAt(ans.Count - 1);
+          } else {
+            ans.Add(c);
+          }
+        }
+
+        return new string(ans.ToArray());
+    }
+}
