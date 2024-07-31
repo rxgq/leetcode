56512f36fc21e815dc0a23b91218368ee02c9203
rxgq <ashtondunderdale@gmail.com>
2024-07-31 22:50:26 +0100

    2670

---
+++ easy/2670.cs
@@ -0,0 +1,17 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int[] DistinctDifferenceArray(int[] nums) {
+        var distinct = new HashSet<int>(nums);
+        var result = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++) {
+          var distinctPrefix = new HashSet<int>(nums[..(i + 1)]);
+          var distinctSuffix = new HashSet<int>(nums[(i + 1)..]);
+
+s          result[i] = distinctPrefix.Count - distinctSuffix.Count;
+        }
+
+        return result;
+    }
+}
