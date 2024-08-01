cc155a0e927228a3c88e48f9fdb114ae0cbc9ba6
rxgq <ashtondunderdale@gmail.com>
2024-08-01 20:08:30 +0100

    2236

---
+++ easy/2236.cs
@@ -0,0 +1,17 @@
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public bool CheckTree(TreeNode root) {
+        return root.left.val + root.right.val == root.val;
+    }
+}
