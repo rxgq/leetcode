546d33b3f20901c9e0e9f12852358be77a4e194c
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-01 16:39:41 +0100

    1137

---
+++ easy/1137.cs
@@ -0,0 +1,19 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int Tribonacci(int n) {
+        if (n == 0) return 0;
+        if (n < 2) return 1;
+
+        int t0 = 0; int t1 = 1; int t2 = 1;
+
+        for (int i = 3; i <= n; i++) {
+            int tn = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tn;
+        }
+
+        return t2;
+    }
+}
