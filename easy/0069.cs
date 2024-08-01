4967a5f9824f5a8a881b4810bed57e197aa782eb
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-01 13:13:45 +0100

    69

--- easy/0069.cs
+++ easy/0069.cs
@@ -1,18 +1,22 @@
 namespace leetcode.easy;
 
 public partial class Solution {
-    public int MySqrt(int x) {
+    public int mySqrt(int x) {
         if (x == 0) return 0;
+        if (x == 1) return 1;
 
-        int left = 0; int right = x;
+        long left = 0;
+        long right = x;
+        
         while (left <= right) {
-            int mid = (left + right) / 2;
-
-            if (mid * mid == x) return mid;
-            else if (mid * mid < x) left = mid + 1;
+            long mid = left + (right - left) / 2;
+            long midSquared = mid * mid;
+            
+            if (midSquared == x) return (int) mid;
+            else if (midSquared < x) left = mid + 1;
             else right = mid - 1;
         }
-
-        return right;
+        
+        return (int)right;
     }
 }
