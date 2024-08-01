371d0a1a85a9068defcab702d730ed3b78d834ab
rxgq <ashtondunderdale@gmail.com>
2024-08-01 18:30:19 +0100

    1572

---
+++ easy/1572.cs
@@ -0,0 +1,15 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int DiagonalSum(int[][] mat) {
+        int sum = 0;
+
+        for (int i = 0; i < mat.Length; i++) {
+          
+          for (int j = 0; j < mat.Length; j++)
+            if (i == j || i + j == mat.Length - 1) sum += mat[i][j];
+        }
+
+        return sum;
+    }
+}
