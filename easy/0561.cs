68930cde681a9f3f40060b29b3cdb8e0a5a84a7c
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-01 11:37:27 +0100

    561

---
+++ easy/0561.cs
@@ -0,0 +1,13 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int ArrayPairSum(int[] nums) {
+        Array.Sort(nums);
+
+        int maxSum = 0;
+        for (int i = 0; i < nums.Length; i += 2)
+            maxSum += nums[i];
+
+        return maxSum;
+    }
+}
