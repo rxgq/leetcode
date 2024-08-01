bc01c28843baa3aeed49dbd6143fa0d8b4063a0a
rxgq <ashtondunderdale@gmail.com>
2024-08-01 21:17:36 +0100

    2427

---
+++ easy/2427.cs
@@ -0,0 +1,12 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int CommonFactors(int a, int b) {
+        int count = 0;
+
+        for (int i = 1; i <= Math.Max(a, b); i++)
+          if (a % i == 0 && b % i == 0) count++;
+
+        return count;
+    }
+}
