0cc6918c088bdda2f667e40abaec77f76d8cff43
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-01 16:32:48 +0100

    258

---
+++ easy/0258.cs
@@ -0,0 +1,9 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int AddDigits(int num) {
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
+    }
+}
