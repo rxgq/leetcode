970f3df63d0a28cd713066aef8ab150ed6db83f9
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-01 15:09:21 +0100

    1679

---
+++ medium/1679.cs
@@ -0,0 +1,27 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int MaxOperations(int[] nums, int k) {
+        int count = 0;
+        var freq = new Dictionary<int, int>();
+
+        foreach (int num in nums) {
+            if (!freq.ContainsKey(num)) freq[num] = 1;
+            else freq[num]++;
+        }
+
+        foreach (int num in freq.Keys.ToList()) {
+            int complement = k - num;
+
+            if (freq.ContainsKey(complement)) {
+                if (num == complement) count += freq[num] / 2;
+                else count += Math.Min(freq[num], freq[complement]);
+
+                freq.Remove(num);
+                freq.Remove(complement);
+            }
+        }
+
+        return count;
+    }
+}
