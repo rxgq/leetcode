996b40e8f3608332bb51b5467eb2a7fe4553fbfa
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-01 11:51:37 +0100

    1 redo

--- easy/0001.cs
+++ easy/0001.cs
@@ -2,15 +2,18 @@ namespace leetcode.easy;
 
 public partial class Solution {
     public int[] TwoSum(int[] nums, int target) {
+        var pairs = nums.Select((num, index) => (Num: num, Index: index)).ToArray();
+        Array.Sort(pairs, (a, b) => a.Num.CompareTo(b.Num));
 
-        for (int i = 0; i < nums.Length; i++) {
-            for (int j = 0; j < nums.Length; j++) {
-                if (nums[i] + nums[j] == target && i != j) {
-                    return new int[] {i, j};
-                }
-            }
+        int left = 0, right = nums.Length - 1;
+
+        while (left < right) {
+            var sum = pairs[left].Num + pairs[right].Num;
+            if (sum == target) return [pairs[left].Index, pairs[right].Index];
+            if (sum < target) left++;
+            else right--;
         }
 
-        return new int[0];
+        return [];
     }
 }
