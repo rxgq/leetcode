2a1ded5c1de13d3e982614204943d99a28752314
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-02 14:49:03 +0100

    1556

---
+++ easy/1556.cs
@@ -0,0 +1,19 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public string ThousandSeparator(int n) {
+        var sb = new StringBuilder();
+        string nStr = n.ToString();
+        
+        int count = 0;
+        for (int i = nStr.Length - 1; i >= 0; i--) {
+            sb.Insert(0, nStr[i]);
+            count++;
+
+            if (count % 3 == 0 && i != 0)
+                sb.Insert(0, '.');
+        }
+
+        return sb.ToString();
+    }
+}
