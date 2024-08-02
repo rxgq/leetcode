47bc21a71679e61950f966b57cd841da032768e5
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-02 16:23:43 +0100

    461

---
+++ easy/0461.cs
@@ -0,0 +1,15 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int HammingDistance(int x, int y) {
+        int res = 0;
+        int num = x ^ y;
+
+        while (num != 0) {
+            res += num % 2;
+            num >>= 1;
+        }
+
+        return res;
+    }
+}
