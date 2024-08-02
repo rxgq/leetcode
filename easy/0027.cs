6529d9d3bd23a323a6bc29c63d2d69dd68b60051
rxgq <ashtondunderdale@gmail.com>
2024-08-02 20:44:26 +0100

    27

---
+++ easy/0027.cs
@@ -0,0 +1,15 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.Length; i++) {
+          if (nums[i] != val) {
+            nums[k] = nums[i];
+            k++;
+          }
+        }
+
+        return k;
+    }
+}
