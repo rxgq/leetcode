d1cb842245c4bf709bf0f036313751650d66d0fa
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-02 10:03:17 +0100

    2089

---
+++ easy/2089.cs
@@ -0,0 +1,14 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public IList<int> TargetIndices(int[] nums, int target) {
+        Array.Sort(nums);
+
+        var indices = new List<int>();
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] == target) indices.Add(i);
+        }
+
+        return indices;
+    }
+}
