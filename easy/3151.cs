b9a57602610e534f2820c1068db88307e9ae1d06
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-02 11:11:05 +0100

    3151

--- easy/3151.cs
+++ easy/3151.cs
@@ -4,12 +4,8 @@ public partial class Solution {
     public bool IsArraySpecial(int[] nums) {
         if (nums.Length == 1) return true;
         
-        for (int i = 1; i < nums.Length; i++) {
-            var p = nums[i - 1] % 2 == 0;
-            var c = nums[i] % 2 == 0;
-
-            if (!(!p && c || p && !c)) return false;
-        }
+        for (int i = 1; i < nums.Length; i++)
+            if ((nums[i - 1] & 1) == (nums[i] & 1)) return false;
 
         return true;
     }
