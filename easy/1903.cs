1d99009cc031921155d94659dba0fc5e07b9dbf1
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-02 13:25:12 +0100

    1903

---
+++ easy/1903.cs
@@ -0,0 +1,10 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public string LargestOddNumber(string num) {
+        for (int i = num.Length - 1; i >= 0; i--)
+            if ((num[i] - '0') % 2 != 0) return num.Substring(0, i + 1);
+
+        return "";
+    }
+}
