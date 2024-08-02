76c3467263cb6fad968e695cae4d51f9bb989f35
rxgq <ashtondunderdale@gmail.com>
2024-08-02 23:02:47 +0100

    1689

--- medium/1689.cs
+++ medium/1689.cs
@@ -1,14 +1,7 @@
-﻿using System;
-namespace leetcode.medium;
+﻿namespace leetcode.medium;
 
-public partial class Solution
-{
-    public int MinPartitions(string n)
-    {
-        int max = 0;
-        foreach (char c in n)
-            max = Math.Max(max, c - '0');
-
-        return max;
+public class Solution {
+    public int MinPartitions(string n) {
+        return n.Max() - '0';
     }
 }
