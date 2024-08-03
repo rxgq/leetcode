1d3edc2efac8eded8fcf912d35c5098985d67973
rxgq <ashtondunderdale@gmail.com>
2024-08-03 13:01:33 +0100

    1266

---
+++ easy/1266.cs
@@ -0,0 +1,16 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int MinTimeToVisitAllPoints(int[][] points) {
+      int total = 0;
+
+      for (int i = 0; i < points.Length - 1; i++) {
+        int xDiff = Math.Abs(points[i + 1][0] - points[i][0]);
+        int yDiff = Math.Abs(points[i + 1][1] - points[i][1]);
+
+        total += Math.Max(xDiff, yDiff);
+      }
+
+      return total;
+    }
+}
