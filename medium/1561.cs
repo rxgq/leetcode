70205182e324e33772eef0bfa04fafb515b65926
rxgq <ashtondunderdale@gmail.com>
2024-08-03 21:27:58 +0100

    1561

---
+++ medium/1561.cs
@@ -0,0 +1,13 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int MaxCoins(int[] piles) {
+        Array.Sort(piles);
+
+        int ans = 0;
+        for(int i = piles.Length / 3; i <  piles.Length; i += 2)
+            ans += piles[i];
+
+        return ans;
+    }
+}
