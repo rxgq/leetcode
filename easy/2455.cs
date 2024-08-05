d98d374dff1657deb05cc4e8003c14fca519ba33
rxgq <ashtondunderdale@gmail.com>
2024-08-05 21:16:52 +0100

    2455

---
+++ easy/2455.cs
@@ -0,0 +1,18 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int AverageValue(int[] nums) {
+        int count = 0;
+        int sum = 0;
+
+        foreach (int num in nums) {
+          if (num % 3 == 0 && num % 2 == 0) {
+           count++;
+           sum += num; 
+          }
+        }
+        
+        if (sum != 0 && count != 0) return sum / count;
+        return 0;
+    }
+}
