4e4bed9d43d92ef2f216fe8770077bdb11da1c9a
rxgq <ashtondunderdale@gmail.com>
2024-08-05 20:28:36 +0100

    342

---
+++ easy/0342.cs
@@ -0,0 +1,12 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public bool IsPowerOfFour(int n) {
+        if (n <= 0) return false;
+
+        while (n % 4 == 0)
+          n /= 4;
+
+        return n == 1;
+    }
+}
