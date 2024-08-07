45f071d6885a13b9be43623d3e9afe24a3e1b9dd
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-07 16:50:51 +0100

    1945

---
+++ easy/1945.cs
@@ -0,0 +1,20 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int GetLucky(string s, int k) {
+        string seq = "";
+        foreach (char c in s) 
+            seq += (int)c - 'a' + 1;
+
+        for (int i = 0; i < k; i++) {
+
+            int sum = 0;
+            foreach (char c in seq)
+                sum += int.Parse(c.ToString());
+
+            seq = sum.ToString();
+        }
+
+        return int.Parse(seq);
+    }
+}
