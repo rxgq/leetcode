4ec5b22ded7abaede659f536a9e9f113f18270d8
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-07 16:29:22 +0100

    171

---
+++ easy/0171.cs
@@ -0,0 +1,11 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int TitleToNumber(string columnTitle) {
+        int ans = 0;
+        foreach (char c in columnTitle)
+            ans = ans * 26 + (c - 'A' + 1);
+
+        return ans;
+    }
+}
