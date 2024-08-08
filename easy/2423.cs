8f3d1d60943d9805bbc130b09b64640658ca5341
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-08 14:38:37 +0100

    2423

---
+++ easy/2423.cs
@@ -0,0 +1,35 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public bool EqualFrequency(string word) {
+        var freq = new Dictionary<char, int>();
+        foreach (char c in word)
+            freq[c] = freq.TryGetValue(c, out int val) ? ++val : 1;
+
+        var freqOfFreq = new Dictionary<int, int>();
+        foreach (int c in freq.Values)
+            freqOfFreq[c] = freqOfFreq.TryGetValue(c, out int val) ? ++val : 1;
+    
+        if (freqOfFreq.Count == 1)
+            return freqOfFreq.ContainsKey(1) && freqOfFreq[1] == 1;
+
+        if (freqOfFreq.Count == 2) {
+            var keys = freqOfFreq.Keys.ToList();
+            int f1 = keys[0], f2 = keys[1];
+            
+            if (f1 > f2) {
+                int temp = f1;
+                f1 = f2;
+                f2 = temp;
+            }
+            
+            if (f1 == 1 && freqOfFreq[f1] == 1)
+                return true;
+            
+            if (f2 == f1 + 1 && freqOfFreq[f2] == 1)
+                return true;
+        }
+        
+        return false;
+    }
+}
