50293c6022224302b397f291af9996bda2f57d22
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-08-08 14:16:22 +0100

    1805

---
+++ easy/1805.cs
@@ -0,0 +1,22 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public int NumDifferentIntegers(string word) {
+        var freq = new HashSet<string>();
+
+        for (int i = 0; i < word.Length; i++) {
+            if (char.IsDigit(word[i])) {
+                var sb = new StringBuilder();
+
+                while (i < word.Length && char.IsDigit(word[i])) {
+                    sb.Append(word[i]);
+                    i++;
+                }
+
+                freq.Add(sb.ToString().TrimStart('0'));
+            }
+        }
+
+        return freq.Count;
+    }
+}
