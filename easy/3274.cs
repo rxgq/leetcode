66f9c2d5045aaf64871ac32912066504aa037393
rxgq <ashtondunderdale@gmail.com>
2024-09-21 13:54:29 +0100

    3274

---
+++ easy/3274.cs
@@ -0,0 +1,7 @@
+namespace leetcode.easy;
+
+public partial class Solution {
+    public bool CheckTwoChessboards(string c1, string c2) {
+        return (c1[0] + c1[1] + c2[0] + c2[1]) % 2 == 0;
+    }
+}
